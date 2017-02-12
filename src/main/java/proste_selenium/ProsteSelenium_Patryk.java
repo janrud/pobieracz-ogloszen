1981f161b4751043c27d97e6538987300858b513
Patryk Czarnik <patryk@patrykczarnik.net>
2017-02-12 12:21:46 +0100

    Prosty program wchodzący na alx.pl

---
+++ src/main/java/proste_selenium/ProsteSelenium_Patryk.java
@@ -0,0 +1,33 @@
+package proste_selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class ProsteSelenium_Patryk {
+
+	public static void main(String[] args) {
+		System.out.println("Startujemy...");
+		//System.setProperty("webdriver.gecko.driver", "C:/Tools/geckodriver.exe");
+		System.setProperty("webdriver.gecko.driver", "/opt/selenium/geckodriver");
+		
+		WebDriver driver = null;
+		
+		try {
+			driver = new FirefoxDriver();
+			
+			driver.get("http://www.alx.pl");
+			
+			System.out.println("Bieżąca strona "
+					+ driver.getCurrentUrl() + " "
+					+ driver.getTitle());
+			
+		} finally {
+			if(driver != null) {
+				driver.quit();
+			}
+		}
+		
+		System.out.println("Koniec programu");
+	}
+
+}
